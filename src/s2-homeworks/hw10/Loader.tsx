aee74155014bcd077d7fab6f709d999e306f4bde
ek-mlg <ek.mlg@icloud.com>
2023-02-11 20:47:55 +0300

    hw10 add loader

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
+import CircularProgress from '@mui/material/CircularProgress';
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    {<CircularProgress color="secondary"/>}
+</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,19 +1,22 @@
+type LoadingActionType = {
+    type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType) => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return {
+                ...state,
+            isLoading: action.isLoading
+            }
         default:
             return state
     }
-}
-
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
